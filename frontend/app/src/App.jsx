852852ee6c3d68ee10b771275e43bf810d59dc78
Sujan <sujanedwin2006@gmail.com>
2024-12-19 13:22:28 +0530

    new

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
+import Home from './home'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-          <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <Router>
+          <Routes>
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
+    </Router>
 
     </>
   )
